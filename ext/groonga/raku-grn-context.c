d4a86082cff56c3a58a2e6051e277c2533d2a525
Yasuhiro Horimoto <horimoto@clear-code.com>
2019-12-16 20:14:51 +0900

    context: add a function for initializing context

---
+++ ext/groonga/raku-grn-context.c
@@ -0,0 +1,27 @@
+/*
+    Copyright (C) 2019 Horimoto Yasuhiro <iddqsar888plekww@gmail.com>
+
+    This file is part of Rakuroonga.
+
+    Rakuroonga is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Rakuroong is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Rakuroonga.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "raku-grn.h"
+
+grn_ctx init_context(void) {
+  grn_ctx ctx;
+  grn_ctx_init(&ctx, 0);
+
+  return ctx;
+}
