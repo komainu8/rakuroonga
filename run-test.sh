b02cbd905e2505f318498c4eefb82786c9c32cd1
Horimoto Yasuhiro <horimoto@clear-code.com>
2020-01-28 21:50:38 +0900

    test: add missing paths

--- run-test.sh
+++ run-test.sh
@@ -6,4 +6,9 @@ rm -rf ./t/db
 ~/rakudo/bin/perl6 02-create_database.t
 ~/rakudo/bin/perl6 03-select.t
 ~/rakudo/bin/perl6 04-full-text-search.t
+~/rakudo/bin/perl6 ./t/00-use.t
+~/rakudo/bin/perl6 ./t/01-new.t
+~/rakudo/bin/perl6 ./t/02-create_database.t
+~/rakudo/bin/perl6 ./t/03-select.t
+~/rakudo/bin/perl6 ./t/04-full-text-search.t
 
