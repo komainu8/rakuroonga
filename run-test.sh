600ecc1e255b7c7d6debdf88e1886fb30bbc4ee3
Horimoto Yasuhiro <horimoto@clear-code.com>
2020-01-28 21:51:04 +0900

    test: add a directory for database

--- run-test.sh
+++ run-test.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-rm -rf ./t/db
-~/rakudo/bin/perl6 00-use.t
-~/rakudo/bin/perl6 01-new.t
-~/rakudo/bin/perl6 02-create_database.t
-~/rakudo/bin/perl6 03-select.t
-~/rakudo/bin/perl6 04-full-text-search.t
+mkdir ./t/db
+
 ~/rakudo/bin/perl6 ./t/00-use.t
 ~/rakudo/bin/perl6 ./t/01-new.t
 ~/rakudo/bin/perl6 ./t/02-create_database.t
 ~/rakudo/bin/perl6 ./t/03-select.t
 ~/rakudo/bin/perl6 ./t/04-full-text-search.t
 
+rm -rf ./t/db
